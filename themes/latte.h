f23be0019ab86106825bf323da1e7d90c5de0fce
nullishamy <git@amyerskine.me>
2024-04-01 22:17:10 +0100

    build: integrate whiskers

--- themes/latte.h
+++ themes/latte.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#4c4f69", "#eff1f5" },
-	[SchemeSel] = { "#4c4f69", "#04a5e5" },
+	[SchemeSel] = { "#eff1f5", "#04a5e5" },
 	[SchemeOut] = { "#000000", "#04a5e5" },
 };
